4aed3aada12b8a53378cd33a633add309e2ce755
Abhi Udayshankar <abhi.udayshankar@kellogg.northwestern.edu>
2025-04-22 16:19:14 +0000

    practice codealong

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -3,10 +3,29 @@
 # ruby 3-arrays.rb
 
 # Arrays are lists of things
+foods = ["tacos","pizza","ice cream"]
+p foods
+
+numbers = [4, 8, 15, 16, 23, 42]
+p numbers
 
 # Accessing the array
+puts foods[0]
+puts foods[1]
+puts foods[2]
+puts foods[3]
+# puts nil
+puts foods[-1] # access the last item in the array
 
 # Add to the array
+shopping_list = foods + ["eggs"]
+shopping_list = foods.push "eggs"
+p shopping_list
+
+shilos_list = ["toys", "pullups"]
+foods.push(shilos_list)
+p foods
+p foods[3][1]
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
